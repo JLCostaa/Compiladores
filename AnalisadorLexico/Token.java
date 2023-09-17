958fa50956a6824ce170d1d6f888c98ee79801e3
JlCosta <Jullia.assis@estudante.ifgoiano.edu.br>
2023-09-17 16:22:22 -0300

    Create Token.java

---
+++ AnalisadorLexico/Token.java
@@ -0,0 +1,17 @@
+
+public class Token {
+    public TipoToken nome;
+    public String lexema;
+
+    public Token(TipoToken nome, String lexema) {
+        this.nome = nome;
+        this.lexema = lexema;
+    }
+
+    // metodo to string para apresentar de forma mais elaborada e facil a represenar
+    // string da classe token
+
+    public String toString(){
+        return "<" + nome + "," + lexema + ">";
+    }
+}
